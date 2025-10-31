2d4cb58e2cf5c61b06e78cd946cdb4df461803f5
Enrico0418 <enrico.marchisio2@mail.dcu.ie>
2025-10-31 15:03:56 +0000

    Update MAG data analysi my iteration.py

--- MAG data analysi my iteration.py
+++ MAG data analysi my iteration.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Oct 31 14:53:14 2025
+
+@author: march
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sat Sep 27 15:30:55 2025
 
 @author: Enrico
@@ -98,7 +105,7 @@ flyby_date_str = flyby_date.strftime("%d %B %Y")
 #timeUTC = df['time']
 
 # Set up the figure with multiple subplots (5 rows, 1 column for magnetic field plots and distance)
-fig, axs = plt.subplots(5, 1, figsize=(10, 20), sharex=True)
+fig, axs = plt.subplots(3, 1, figsize=(15, 10), sharex=True)
 fig.suptitle(f'Galileo MAG for {title_label} on {flyby_date_str}')
 plt.tight_layout(pad=2.0)  # reduce padding between title and subplots
 
@@ -124,18 +131,6 @@ axs[2].set_ylabel('Bz (nT)')
 axs[2].tick_params(axis='x', rotation=45)
 axs[2].grid(True)  # Add grid to the third subplot
 
-# Plot for B Total
-axs[3].plot(timeUTC, BTot, label='B Total (nT)', color='black', linestyle='--')
-axs[3].set_ylabel('B Total (nT)')
-axs[3].tick_params(axis='x', rotation=45)
-axs[3].grid(True)  # Add grid to the fourth subplot
-
-# Plot for Distance from Europa's surface
-axs[4].plot(timeUTC, distance_from_surface, label='Distance from Europa surface (km)', color='blue')
-axs[4].set_xlabel('Time (UTC Hours)')
-axs[4].set_ylabel('Distance From Surface($R_Moon$)')
-axs[4].tick_params(axis='x', rotation=45)
-axs[4].grid(True)  # Add grid to the fifth subplot
 
 # Add a vertical dashed line for Closest Approach (CA) on each subplot
 closest_approach_time = timeUTC[np.argmin(distance_from_surface)]  # Find time of closest approach
@@ -145,13 +140,13 @@ for i, ax in enumerate(axs):
 
 
 # Now, plot the 3D trajectory projections in a separate figure
-fig2, axs2 = plt.subplots(1, 3, figsize=(18, 6))
-fig2.suptitle(f'Galileo MAG for {title_label} on {flyby_date_str}')
+fig2, axs2 = plt.subplots(1, 2, figsize=(15, 8))
+fig2.suptitle(f'Galileo MAG for {title_label} on {flyby_date_str}', fontsize = 20)
 
 # Plot the trajectory in the xy-plane (no gradient, simple scatter plot)
 axs2[0].scatter(x, y, color='black', s=10, label='Trajectory (xy-plane)')
-axs2[0].set_xlabel('X ($R_Moon$)')
-axs2[0].set_ylabel('Y ($R_Moon$)')
+axs2[0].set_xlabel('X ($R_Moon$)', fontsize = 20)
+axs2[0].set_ylabel('Y ($R_Moon$)', fontsize = 20)
 moon_circle = Circle((0, 0), 1, color='black', fill=False, linewidth=2, label='Europa')  # Create a new moon circle for this plot
 axs2[0].add_patch(moon_circle)  # Add the moon
 axs2[0].set_xlim([-6.5, 6.5])  # Set the range of x-axis from -6 to 6
@@ -160,23 +155,14 @@ axs2[0].grid(True)  # Add grid to the first 3D projection plot
 
 # Plot the trajectory in the yz-plane (no gradient, simple scatter plot)
 axs2[1].scatter(y, z, color='black', s=10, label='Trajectory (yz-plane)')
-axs2[1].set_xlabel('Y ($R_Moon$)')
-axs2[1].set_ylabel('Z ($R_Moon$)')
+axs2[1].set_xlabel('Y ($R_Moon$)', fontsize = 20)
+axs2[1].set_ylabel('Z ($R_Moon$)', fontsize = 20)
 moon_circle = Circle((0, 0), 1, color='black', fill=False, linewidth=2, label='Europa')  # Create a new moon circle for this plot
 axs2[1].add_patch(moon_circle)  # Add the moon
 axs2[1].set_xlim([-4, 4])  # Set the range of y-axis from -6 to 6
 axs2[1].set_ylim([-4, 4])  # Set the range of z-axis from -6 to 6
 axs2[1].grid(True)  # Add grid to the second 3D projection plot
 
-# Plot the trajectory in the xz-plane (no gradient, simple scatter plot)
-axs2[2].scatter(x, z, color='black', s=10, label='Trajectory (xz-plane)')
-axs2[2].set_xlabel('X ($R_Moon$)')
-axs2[2].set_ylabel('Z ($R_Moon$)')
-moon_circle = Circle((0, 0), 1, color='black', fill=False, linewidth=2, label='Europa')  # Create a new moon circle for this plot
-axs2[2].add_patch(moon_circle)  # Add the moon
-axs2[2].set_xlim([-6.5, 6.5])  # Set the range of x-axis from -6 to 6
-axs2[2].set_ylim([-6.5, 6.5])  # Set the range of z-axis from -6 to 6
-axs2[2].grid(True)  # Add grid to the third 3D projection plot
 
 # Adjust layout for the 3D projection plots
 plt.tight_layout()
